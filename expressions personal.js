23b0a3218aeb3c7906e134028417fad85e59aba1
JamesMcGorty <jbmcgorty@gmail.com>
2014-03-13 09:59:28 -0400

    javascrpit6

--- expressions personal.js
+++ expressions personal.js
@@ -4,10 +4,21 @@
 function averagecalc() 
 { 
 
-var number1=prompt("Please enter number 1") 
-number1=pareslnt(number1);// Convert to type :'int'
-var number2=prompt("Please enter number 2") 
-number2=pareslnt(number2);// Convert to type :'int'
+number1 = prompt("Please enter number 1"); 
+number1 = parseInt(number1); // Convert to type 'int' 
+number2 = prompt("Please enter number 2"); 
+number2 = parseInt(number2); // Convert to type 'int' 
+number3 = prompt("Please enter number 3"); 
+number3 = parseInt(number1); // Convert to type 'int' 
+number4 = prompt("Please enter number 4"); 
+number4 = parseInt(number2); // Convert to type 'int' 
+number5 = prompt("Please enter number 5"); 
+number5 = parseInt(number1); // Convert to type 'int' 
+number6 = prompt("Please enter number 6"); 
+number6 = parseInt(number2); // Convert to type 'int' 
+number7 = prompt("Please enter number 7"); 
+number7 = parseInt(number1); // Convert to type 'int' 
+
 average=(number1+number2)/2 
 
 } 
