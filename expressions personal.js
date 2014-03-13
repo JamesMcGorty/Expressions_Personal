660bb33f2418e6c75343385e6c299c60a29e60d8
JamesMcGorty <jbmcgorty@gmail.com>
2014-03-13 09:24:49 -0400

    javascript3

--- expressions personal.js
+++ expressions personal.js
@@ -4,8 +4,8 @@
 function averagecalc() 
 { 
 
-number1=prompt("Please enter number 1") 
-number2=prompt("Please enter number 2") 
+var number1=prompt("Please enter number 1") 
+var number2=prompt("Please enter number 2") 
 average=(number1+number2)/2 
 
 } 
