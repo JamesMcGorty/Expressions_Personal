52c13ad56daed6781df3efbeb7a86ecbded31197
JamesMcGorty <jbmcgorty@gmail.com>
2014-03-13 09:29:02 -0400

    javascript5

--- expressions personal.js
+++ expressions personal.js
@@ -7,6 +7,7 @@ function averagecalc()
 var number1=prompt("Please enter number 1") 
 number1=pareslnt(number1);// Convert to type :'int'
 var number2=prompt("Please enter number 2") 
+number2=pareslnt(number2);// Convert to type :'int'
 average=(number1+number2)/2 
 
 } 
